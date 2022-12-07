f054f13e7f70ec565432a2d4a2659b401308177a
João Henrique Terêncio <94974380+Jh-Terencio@users.noreply.github.com>
2022-12-07 00:38:08 -0300

    first commit

---
+++ src/components/Botao/index.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import style from './Botao.module.scss';
+
+interface Props{
+    onClick?: () => void,
+    type?: "button"|"submit"|"reset"|undefined,
+    children?: React.ReactNode
+}
+
+function Botao({ onClick, type, children }: Props){
+    return(
+        <button 
+        onClick={onClick} 
+        type={type} 
+        className={style.botao}
+        >
+            {children}
+        </button>
+    )
+}
+
+export default Botao;
